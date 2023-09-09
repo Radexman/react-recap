bcb25e7be96fb50cc28bb8005f453f63b90a208d
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-09-09 13:29:10 +0200

    Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
-const App = () => {
-	const title = 'Blog Post';
-	const body = 'This is my blog post';
-	const comments = [
-		{ id: 1, text: 'Comment one' },
-		{ id: 2, text: 'Comment two' },
-		{ id: 3, text: 'Comment three' },
-	];
+import Header from './components/Header';
 
+const App = () => {
 	return (
-		<div>
-			<h2>{title.toUpperCase()}</h2>
-			<p>{body}</p>
-			<div>
-				<h3>Comments ({comments.length})</h3>
-				<ul>
-					{comments.map((comment, idx) => {
-						const { text } = comment;
-						return <li key={idx}>{text}</li>;
-					})}
-				</ul>
+		<>
+			<Header />
+			<div className='container'>
+				<h1>My App</h1>
 			</div>
-		</div>
+		</>
 	);
 };
 
