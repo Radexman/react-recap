b178835eb5d94c20ff72411b33bb827b8ee7c6e0
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-09-09 12:41:52 +0200

    Dynamic Values

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
 const App = () => {
-	return <h1>Hello from the app component</h1>;
+	const title = 'Blog Post';
+	const body = 'This is my blog post';
+	const comments = [
+		{ id: 1, text: 'Comment one' },
+		{ id: 2, text: 'Comment two' },
+		{ id: 3, text: 'Comment three' },
+	];
+
+	return (
+		<div>
+			<h2>{title.toUpperCase()}</h2>
+			<p>{body}</p>
+			<div>
+				<h3>Comments ({comments.length})</h3>
+				<ul>
+					{comments.map((comment, idx) => {
+						const { text } = comment;
+						return <li key={idx}>{text}</li>;
+					})}
+				</ul>
+			</div>
+		</div>
+	);
 };
 
 export default App;
