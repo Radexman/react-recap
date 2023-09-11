5d280039f769e4ec11be3ca99ca41c998191fab2
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-09-11 11:40:24 +0200

    React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { v4 as uuidv4 } from 'uuid';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import AboutPage from './components/Pages/AboutPage';
 import data from './data';
 
 const App = () => {
@@ -21,17 +23,34 @@ const App = () => {
 	};
 
 	return (
-		<>
-			<Header />
-			<div className='container'>
-				<FeedbackForm handleAdd={addFeedback} />
-				<FeedbackStats feedback={feedback} />
-				<FeedbackList
-					feedback={feedback}
-					handleDelete={deleteFeedback}
-				/>
-			</div>
-		</>
+		<Router>
+			<>
+				<Header />
+				<div className='container'>
+					<Routes>
+						<Route
+							exact
+							path='/'
+							element={
+								<>
+									<FeedbackForm handleAdd={addFeedback} />
+									<FeedbackStats feedback={feedback} />
+									<FeedbackList
+										feedback={feedback}
+										handleDelete={deleteFeedback}
+									/>
+								</>
+							}
+						></Route>
+
+						<Route
+							path='/about'
+							element={<AboutPage />}
+						/>
+					</Routes>
+				</div>
+			</>
+		</Router>
 	);
 };
 
